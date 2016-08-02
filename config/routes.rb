37c785f46adf123a3801eaeba65ea614405f9ddd
nhannt <nhannt@zigexn.vn>
2016-08-02 16:50:09 +0700

    Implement advanced login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  root 'static_page#home'
-  get  '/help', to: 'static_page#help'
-  #get  '/about',   to: 'static_page#about'
-  #get  '/contact', to: 'static_page#contact'
-  get  '/signup',  to: 'users#new'
+  get 'sessions/new'
+
+  root 	 'static_page#home'
+  get  	 '/help', 	 to: 'static_page#help'
+  #get   '/about',   to: 'static_page#about'
+  #get   '/contact', to: 'static_page#contact'
+  get  	 '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :users
 
 end
