19c2d018c1e4c35dc526775e02de267056dfea4a
nhannt <nhannt@zigexn.vn>
2016-08-02 15:53:49 +0700

    Finish signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'static_page#home'
   get  '/help', to: 'static_page#help'
-  #get  '/about',   to: 'static_pages#about'
-  #get  '/contact', to: 'static_pages#contact'
+  #get  '/about',   to: 'static_page#about'
+  #get  '/contact', to: 'static_page#contact'
   get  '/signup',  to: 'users#new'
-  resource :users
+  resources :users
+
 end
