c7ad0570ee3f364997acedab6fed15e85c97dcf1
nhannt <nhannt@zigexn.vn>
2016-08-16 14:35:46 +0700

    add before_action to OrderItemsController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_order
 
   def current_order
-    Order.includes(:order_items).find_by(id: session[:order_id]) || Order.new
+    Order.find_by(id: session[:order_id]) || Order.new
   end
 
 end
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,4 +1,5 @@
 class OrderItemsController < ApplicationController
+  before_action :check_item_exists, only: :create
   def create
     @order = current_order
     @order_item = @order.order_items.new(order_item_params)
@@ -9,19 +10,28 @@ class OrderItemsController < ApplicationController
   def update
     @order = current_order
     @order_item = @order.order_items.find(params[:id])
-    @order_item.update_attributes(order_item_params)
+    @order_item.update_attributes!(order_item_params)
     @order_items = @order.order_items
   end
 
   def destroy
     @order = current_order
     @order_item = @order.order_items.find(params[:id])
-    @order_item.destroy
+    @order_item.destroy!
     @order_items = @order.order_items
   end
 
 private
   def order_item_params
     params.require(:order_item).permit(:quantity, :product_id)
+  end
+  # Check item exists in order_items.
+  def check_item_exists
+    store_location
+    @order = current_order
+    if @order_item = @order.order_items.find_by(product_id: params['order_item']['product_id'])
+      flash[:danger] = "Item already added to cart!"
+      redirect_to cart_url
+    end
   end
 end
