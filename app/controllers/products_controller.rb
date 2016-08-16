b471cea2a8c58e3f6fe305bbc3eef8c14d767288
nhannt <nhannt@zigexn.vn>
2016-08-16 15:21:16 +0700

    update products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,12 @@
 class ProductsController < ApplicationController
   def index
-  	@categories = Category.all
-  	@category = Category.exists?(params[:category]) ? Category.find(params[:category]) : false
-  	if @category
-  		@products = @category.products.paginate(page: params[:page], per_page: 8)
-  	else
+    @categories = Category.all
+    @category = @categories.find { |category| category.id = params[:category] }
+    if @category
+      @products = @category.products.paginate(page: params[:page], per_page: 8)
+    else
       @products = Product.paginate(page: params[:page], per_page: 8)
-     end
+    end
     @order_item = current_order.order_items.new
   end
 end
