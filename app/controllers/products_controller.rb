ec8aba923bbecc19af562a46d51e79ed2a2231e2
nhannt <nhannt@zigexn.vn>
2016-08-17 09:14:58 +0700

    update products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
   def index
     @categories = Category.all
-    @category = @categories.find { |category| category.id = params[:category] }
+    @category = @categories.find { |item| item.id.to_s == params[:category] }
     if @category
-      @products = @category.products.paginate(page: params[:page], per_page: 8)
+      @products = Product.where( category_id: @category.id ).paginate( page: params[:page], per_page: 8 )
     else
-      @products = Product.paginate(page: params[:page], per_page: 8)
+      @products = Product.paginate( page: params[:page], per_page: 8 )
     end
     @order_item = current_order.order_items.new
   end
