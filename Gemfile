43e62c9d97ee7390b8cd35f066a28c1fdcdf474f
nhannt <nhannt@zigexn.vn>
2016-08-26 16:13:12 +0700

    implement search feature

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'will_paginate', '3.1.0'
 gem 'bootstrap-will_paginate', '0.0.10'
 gem 'bootstrap-sass', '3.3.6'
 gem 'vacuum', '1.3.0'
+gem 'sunspot_rails'
+gem 'rsolr', '1.1.1'
 #gem 'autoprefixer-rails'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
@@ -48,6 +50,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'sunspot_solr'
 end
 
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,27 @@
 class ProductsController < ApplicationController
+  before_action :getCategoryAndOrderItem
   def index
-    @categories = Category.all
-    @category = @categories.find { |item| item.id.to_s == params[:category] }
     if @category
       @products = Product.where( category_id: @category.id ).paginate( page: params[:page], per_page: 8 )
     else
       @products = Product.paginate( page: params[:page], per_page: 8 )
     end
+  end
+
+  def search
+    # @products = ProductsSolrConnection.get 'select', :params => {:q => '*:*'}
+    search_params = { q: "name:#{params[:search]}", sort: 'created_at desc' }
+    @response = ProductsSolrConnection.get( 'select', params: search_params )
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+private
+  def getCategoryAndOrderItem
+    @categories = Category.all
+    @category = @categories.find { |item| item.id.to_s == params[:category] }
     @order_item = current_order.order_items.new
   end
 end
